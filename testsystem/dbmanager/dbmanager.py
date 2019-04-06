0bea92831815bf4e1003ca220e7513420158f15b
JohnTer <dj.vadim@list.ru>
2019-04-06 17:13:44 +0300

    Add test dbmanager and add ext methods to dbmanager. Ticket: EJ-54

--- testsystem/dbmanager/dbmanager.py
+++ testsystem/dbmanager/dbmanager.py
@@ -3,82 +3,104 @@ import tablestructures.answers
 import tablestructures.solution
 import tablestructures.tasklimits
 import tablestructures.tests
+import uuid
 
 class BDManager():
-    def __init__(self,dbname = "ejudge_test",usr = "admin",psw = "admin",host = "localhost"):
-        self.connection = psycopg2.connect(database=dbname, user=usr, host=host, password=psw)
+    def __init__(self,dbname = "ejudge_test",usr = "admin",psw = "admin",host = "localhost", port = "5332"):
+        self.connection = psycopg2.connect(database=dbname, user=usr, host=host, password=psw, port=port)
+        self.connection.autocommit = True
 
-
-    def __make_request(self, request, params):
+    def _make_request(self, request, params, insert = False):
         with self.connection.cursor() as cursor:
             cursor.execute(request, params)
-            result = cursor.fetchall()
+            if not insert:
+                result = cursor.fetchall()
+            else:
+                result = None
         return result
 
-    def __get_answer(self, answer_id):
-        request = """SELECT * FROM answers WHERE id = %(answer_id)s"""
+    def _get_answer(self, answer_id):
+        request = """SELECT programming_language, program_text FROM answers WHERE id = %(answer_id)s"""
         params = {'answer_id': answer_id}
-        results = self.__make_request(request, params)
+        results = self._make_request(request, params)
 
         if len(results) == 0:
             return None
         else:
             result = results[0]
-            return tablestructures.answers.Answers(result[1], result[2])
+            return tablestructures.answers.Answers(result[0], result[1])
 
-    def __get_task_limit(self, task_id, programming_language):
-        request = """SELECT * FROM task_limits WHERE id = %(task_id)s AND programming_language = %(programming_language)s"""
+    def _get_task_limit(self, task_id, programming_language):
+        request = """SELECT memory_limit, time_limit, programming_language FROM task_limits WHERE id = %(task_id)s AND programming_language = %(programming_language)s"""
         params = {'task_id': task_id, 'programming_language': programming_language}
-        results = self.__make_request(request, params)
+        results = self._make_request(request, params)
 
         if len(results) == 0:
             return None
         else:
             result = results[0]
-            return tablestructures.tasklimits.TaskLimits(result[1], result[2], result[3])
+            return tablestructures.tasklimits.TaskLimits(result[0], result[1], result[2])
 
-    def __get_tests(self, task_id):
-        request = """SELECT * FROM tests WHERE id = %(task_id)s"""
+    def _get_tests(self, task_id):
+        request = """SELECT input_data, output_data FROM tests WHERE task_id = %(task_id)s"""
         params = {'task_id': task_id}
-        results = self.__make_request(request, params)
+        results = self._make_request(request, params)
 
         if len(results) == 0:
             return None
         else:
-            return [tablestructures.tests.Tests(test[1], test[2]) for test in results]
+            return [tablestructures.tests.Tests(test[0], test[1]) for test in results]
 
-    def __get_taskid(self, user_solution_id):
+    def _get_taskid(self, user_solution_id):
         request = """SELECT task_id FROM user_solutions WHERE id = %(user_solution_id)s"""
         params = {'user_solution_id': user_solution_id}
-        results = self.__make_request(request, params)
+        results = self._make_request(request, params)
 
         if len(results) == 0:
             return None
         else:
-            return results[0]
+            return results[0][0]
 
-    def __get_answerid(self, user_solution_id):
+    def _get_answerid(self, user_solution_id):
         request = """SELECT answer_id FROM user_solutions WHERE id = %(user_solution_id)s"""
         params = {'user_solution_id': user_solution_id}
-        results = self.__make_request(request, params)
+        results = self._make_request(request, params)
 
         if len(results) == 0:
             return None
         else:
-            return results[0]
+            return results[0][0]
 
 
 
     def get_solution(self, user_solution_id):
-        answer_id = self.__get_answerid(user_solution_id)
-        task_id = self.__get_taskid(user_solution_id)
+        answer_id = self._get_answerid(user_solution_id)
+        task_id = self._get_taskid(user_solution_id)
 
-        answer = self.__get_answer(answer_id)
-        task_limit = self.__get_task_limit(task_id, answer.programming_language)
-        tests = self.__get_tests(task_id)
+        answer = self._get_answer(answer_id)
+        task_limit = self._get_task_limit(task_id, answer.programming_language)
+        tests = self._get_tests(task_id)
 
         solution = tablestructures.solution.Solution(user_solution_id, answer, tests, task_limit)
         return solution
+
+    def _add_status(self, status_id, result, error_test_id, ext_info = ""):
+        request = """INSERT INTO status (id, result, error_test_id, extended_information) VALUES (%(status_id)s,%(result)s,%(error_test_id)s,%(ext_info)s)"""
+        params = {'status_id': status_id, 'result': result, 'error_test_id': error_test_id, 'ext_info': ext_info }
+        self._make_request(request, params, True)
+
+    def _update_status_id(self, user_solution_id, status_id):
+        request = """UPDATE user_solutions SET status_id=%(status_id)s WHERE id=%(user_solution_id)s"""
+        params = {'user_solution_id': user_solution_id, 'status_id': status_id}
+        self._make_request(request, params, True)
+
+    def add_status(self, user_solution_id, result, error_test_id, ext_info = ""):
+        status_id = str(uuid.uuid1())
+        self._add_status(status_id,result,error_test_id,ext_info)
+        self._update_status_id(user_solution_id, status_id)
+        return status_id
+
+
 
 
 
---
+++ testsystem/dbmanager/tests/test_dbmanager.py
@@ -0,0 +1,101 @@
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+import uuid
+import dbmanager
+import unittest
+class TestAppBuilder(unittest.TestCase):
+    def setUp(self):
+        self.bd = dbmanager.BDManager("ejudge_test", "postgres", "0000", port="5433")
+
+    def test_get_answer(self):
+        answer_id = "1f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        result = self.bd._get_answer(answer_id)
+        self.assertEqual(result.programming_language, "python")
+        self.assertEqual(result.program_text, "import sys\nprint(sys.argv[1:])")
+
+    def test_get_answer_id(self):
+        user_solution_id = "1f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        result = self.bd._get_answerid(user_solution_id)
+        self.assertEqual(result, "1f94dffc-d71b-4b3e-84d5-96a4778dbc11")
+        
+
+    def test_get_task_id(self):
+        user_solution_id = "1f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        result = self.bd._get_taskid(user_solution_id)
+        self.assertEqual(result, "1f94dffc-d71b-4b3e-84d5-96a4778dbc11")
+
+    def test_get_tests(self):
+        task_id = "2f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        result = self.bd._get_tests(task_id)
+
+        self.assertEqual(result[0].input_data, "1 2 3")
+        self.assertEqual(result[0].output_data, "2 4 6")
+
+        self.assertEqual(result[1].input_data, "120")
+        self.assertEqual(result[1].output_data, "240")      
+
+    def test_get_task_limit(self):
+        task_id = "1f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        programming_language = "python"
+        result = self.bd._get_task_limit(task_id, programming_language)
+
+        self.assertEqual(result.time_limit, 10)
+        self.assertEqual(result.memory_limit, 40)
+        self.assertEqual(result.programming_language, "python")
+
+    def test_get_solution_id(self):
+        user_solution_id = "1f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        result = self.bd.get_solution(user_solution_id)
+        self.assertIsNotNone(result)
+
+    
+    def test_add_status_ok(self):
+        temp_uuid = str(uuid.uuid1())
+        self.bd._add_status(temp_uuid, "ok", None, """{}""")
+
+        result = self.bd._make_request("SELECT id, result, error_test_id, extended_information FROM status WHERE id=%(uuid_id)s",{"uuid_id": temp_uuid})
+        result = result[0]
+        self.assertEqual(result[0], temp_uuid)
+        self.assertEqual(result[1], "ok")
+        self.assertEqual(result[2], None)
+        self.assertEqual(result[3], {})
+
+    def test_update_solution_id(self):
+        user_solution_id = "2f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        updt_uuid = str(uuid.uuid1())
+
+        self.bd._update_status_id(user_solution_id, updt_uuid)
+
+        result = self.bd._make_request("SELECT status_id FROM user_solutions WHERE id=%(user_solution_id)s",{"user_solution_id": user_solution_id})
+        result = result[0]
+        self.assertEqual(result[0], updt_uuid)
+
+    def _test_add_status(self):
+        user_solution_id = "2f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        result = "err"
+        error_test_id = "2f94dffc-d71b-4b3e-84d5-96a4778dbc11"
+        ext = {"gcc": "ERROR!!!"}
+        status_id = self.bd.add_status(user_solution_id, result, error_test_id, ext)
+
+
+        result = self.bd._make_request("SELECT status_id FROM user_solutions WHERE id=%(user_solution_id)s",{"user_solution_id": user_solution_id})
+        result = result[0]
+        self.assertEqual(result[0], status_id)
+        
+
+        result = self.bd._make_request("SELECT id, result, error_test_id, extended_information FROM status WHERE id=%(uuid_id)s",{"uuid_id": temp_uuid})
+        result = result[0]
+        self.assertEqual(result[0], status_id)
+        self.assertEqual(result[1], "err")
+        self.assertEqual(result[2], error_test_id)
+        self.assertEqual(result[3], ext)
+
+
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
