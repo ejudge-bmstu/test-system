e695d82f31a7576d313a6660af73651f1ab34e81
JohnTer <dj.vadim@list.ru>
2019-04-11 21:55:42 +0300

    Fix test

--- testsystem/test_main_server.py
+++ testsystem/test_main_server.py
@@ -124,6 +124,7 @@ class TestAppBuilder(unittest.TestCase):
 if __name__ == "__main__":
     calcTestSuite = unittest.TestSuite()
     calcTestSuite.addTest(unittest.makeSuite(TestAppBuilder))
+    print("count_of_tests:" + str(len(calcTestSuite._tests[0]._tests)))
     runner = unittest.TextTestRunner(verbosity=2, stream=sys.stdout)
     result = runner.run(calcTestSuite)
     unittest.main(exit=False)
